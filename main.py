ce116de7383e5f56f22e3ecbd847b30471a4d658
eunchaipark <qkrdmsco578198@gmail.com>
2026-06-10 09:42:28 +0900

    - prefix사용

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from sqlalchemy import text
 from db import engine
 from contextlib import asynccontextmanager
 from batch_recommendation.api.recommendation_router import router as recommendation_router
+from realtime_log.api.log_router import router as log_router
+from food_search.api.search_router import router as search_router
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -17,6 +19,8 @@ async def lifespan(app: FastAPI):
 app = FastAPI(title="Bapsang API", lifespan=lifespan)
 
 app.include_router(recommendation_router, prefix="/recommendations", tags=["recommendations"])
+app.include_router(log_router, prefix="/logs", tags=["logs"])
+app.include_router(search_router, prefix="/search", tags=["search"])
 
 @app.get("/health")
 def health():
@@ -30,11 +34,3 @@ def health_db():
         return {"status": "ok", "db": "connected"}
     except Exception as e:
         return {"status": "error", "db": str(e)}
-    
-
-# router 연결
-from realtime_log.api.log_router import router as log_router
-from food_search.api.search_router import router as search_router
-
-app.include_router(log_router)
-app.include_router(search_router)
